02e4b661345a48df3ced8ece91ff873b1aefce2e
Tony Wang <tony.wang@tum.de>
2023-06-22 14:59:10 +0200

    added breakpoint

--- lib/loss_helper.py
+++ lib/loss_helper.py
@@ -325,6 +325,7 @@ def compute_reference_loss(data_dict, config, no_reference=False):
             # max_sem_cls_prob, _ = data_dict["sem_cls_prob"].max(dim=2)
             # comparison_result = max_sem_cls_prob <= (1 - data_dict["objectness_scores"]).squeeze(dim=2)
             # objectness_masks = comparison_result.float().cpu().numpy()
+            import ipdb; ipdb.set_trace()
             expanded_objectness_scores = 1 - (data_dict["objectness_scores"].expand(-1, -1, 18))
             comparison = data_dict["sem_cls_prob"] > expanded_objectness_scores
             max_comparison = torch.max(comparison, dim=2)[0]
