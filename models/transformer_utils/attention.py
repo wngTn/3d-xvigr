e2e462f5b9868e6316e2c0e700c5f64dd4df04bc
Tony Wang <tony.wang@tum.de>
2023-06-28 16:45:55 +0200

    change prenorm to postnorm

--- models/transformer_utils/attention.py
+++ models/transformer_utils/attention.py
@@ -166,9 +166,9 @@ class MultiHeadAttention(nn.Module):
             out = self.attention(q_norm, k_norm, v_norm, attention_mask, attention_weights, way)
             out = queries + self.dropout(torch.relu(out))
         else:
-            out = self.layer_norm(queries)
             out = self.attention(queries, keys, values, attention_mask, attention_weights, way)
             out = self.dropout(out)
+            out = self.layer_norm(queries + out)
         return out
 
     def forward_faster(self, queries, keys, values, attention_pos, attention_weights, way):
